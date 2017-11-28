e65432c48d5ae730501d86308fe8db6e6b0aebd5
Adrian Bury <kubaadrian2@gmail.com>
2017-11-28 23:00:55 +0100

    Create empty input generator

---
+++ src/app/shared/components/input-generator/input-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inz-input-generator',
+  templateUrl: './input-generator.component.html',
+  styleUrls: ['./input-generator.component.sass']
+})
+export class InputGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
