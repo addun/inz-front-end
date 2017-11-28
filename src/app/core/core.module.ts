08a757e9614cf8c115717f262912ac15eb64b11b
Adrian Bury <kubaadrian2@gmail.com>
2017-11-28 23:17:02 +0100

    Fix options url

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ApiService} from './api/api.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     ApiService
