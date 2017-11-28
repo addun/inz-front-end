012627c0d4e2677c688f16f5842090412685632f
Adrian Bury <kubaadrian2@gmail.com>
2017-11-28 20:34:27 +0100

    Create ApiSerivce

---
+++ src/app/core/api/api.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class ApiService {
+  private serverUrl = environment.apiBaseUrl;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  get <T>(url: string, options?: Options): Observable<any> {
+    return this.httpClient
+      .get(`${this.serverUrl}${url}`, options);
+  }
+
+  post<T>(url: string, body: any, options?: Options): Observable<any> {
+    return this.httpClient
+      .post(`${this.serverUrl}${url}`, body, options);
+  }
+
+  put<T>(url: string, body: any, options?: Options): Observable<any> {
+    return this.httpClient
+      .put(`${this.serverUrl}${url}`, body, options);
+  }
+
+  delete<T>(url: string): Observable<any> {
+    return this.httpClient
+      .delete(`${this.serverUrl}${url}`);
+  }
+
+  patch<T>(url: string, body: any, options?: Options): Observable<any> {
+    return this.httpClient
+      .patch(`${this.serverUrl}${url}`, body, options);
+  }
+
+}
+
+
+class Options {
+  headers?: HttpHeaders;
+  observe?: 'body';
+  params?: HttpParams;
+  reportProgress?: boolean;
+  responseType?: 'json';
+  withCredentials?: boolean;
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ApiService} from './api/api.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  providers: [
+    ApiService
+  ],
   declarations: []
 })
-export class CoreModule { }
+export class CoreModule {
+}
