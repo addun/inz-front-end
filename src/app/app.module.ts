dc8919a94ed7a1a6f659a63980d7c190d805f66a
Adrian Bury <kubaadrian2@gmail.com>
2017-11-28 20:30:31 +0100

    Create LayoutModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
+import {LayoutModule} from './layout/layout.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import {CoreModule} from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
