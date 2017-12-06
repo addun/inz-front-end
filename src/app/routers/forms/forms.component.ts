c8a58934bfcdebcda05d36e5a9122791624aa7af
Adrian Bury <kubaadrian2@gmail.com>
2017-12-06 22:31:52 +0100

    create forms component with module

---
+++ src/app/routers/forms/forms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inz-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.sass']
+})
+export class FormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
