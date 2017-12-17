7491fa506d4d5e83cea4904ca4dfa89461dc1508
Adrian Bury <kubaadrian2@gmail.com>
2017-12-17 13:34:43 +0100

    Add ng-bootstrap

--- src/app/routers/forms/shared/forms-shared.module.ts
+++ src/app/routers/forms/shared/forms-shared.module.ts
@@ -4,11 +4,15 @@ import {FormHeadingComponent} from './components/form-heading/form-heading.compo
 import {DynamicFormsBootstrapUIModule} from '@ng-dynamic-forms/ui-bootstrap';
 import {DynamicFormsCoreModule} from '@ng-dynamic-forms/core';
 import {AddItemComponent} from './components/add-item/add-item.component';
+import {DynamicFormsNGBootstrapUIModule} from '@ng-dynamic-forms/ui-ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgbModule.forRoot(),
     DynamicFormsCoreModule.forRoot(),
+    DynamicFormsNGBootstrapUIModule,
     DynamicFormsBootstrapUIModule,
   ],
   declarations: [
@@ -18,8 +22,11 @@ import {AddItemComponent} from './components/add-item/add-item.component';
   exports: [
     FormHeadingComponent,
     AddItemComponent,
+
+    NgbModule,
     DynamicFormsCoreModule,
     DynamicFormsBootstrapUIModule,
+    DynamicFormsNGBootstrapUIModule,
   ]
 })
 export class FormsSharedModule {
