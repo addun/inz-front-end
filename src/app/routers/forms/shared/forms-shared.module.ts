bf55eb523b6afdb369effb2bebf84913f2ef3802
Adrian Bury <kubaadrian2@gmail.com>
2017-12-31 10:53:23 +0100

    remove unused components

--- src/app/routers/forms/shared/forms-shared.module.ts
+++ src/app/routers/forms/shared/forms-shared.module.ts
@@ -1,21 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {AddItemComponent} from './components/add-item/add-item.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
 import {KeysPipe} from './pipes/keys/keys.pipe';
 import {CardComponent} from './components/card/card.component';
 import {MachineToolSpecificationFormComponent} from './forms/machine-tool-specification-form/machine-tool-specification-form.component';
 import {MachiningCapabilityFormComponent} from './forms/machining-capability-form/machining-capability-form.component';
-import {FieldsetComponent} from './components/fieldset/fieldset.component';
 import {RouterOutletComponent} from './components/router-outlet/router-outlet.component';
 import {SharedModule} from '../../../shared/shared.module';
-import {CardActionComponent} from './components/card-action/card-action.component';
-import {ArrayActionComponent} from './components/array-action/array-action.component';
 import {ChuckFormComponent} from './forms/chuck-form/chuck-form.component';
-import {FormGroupComponent} from './components/form-group/form-group.component';
 import {InputComponent} from './components/input/input.component';
-import {SelectComponent} from './components/select/select.component';
 import {FormComponent} from './components/form/form.component';
 import {DeviceIdControlsComponent} from './controls/device-id-controls/device-id-controls.component';
 import {LocationControlsComponent} from './controls/location-controls/location-controls.component';
@@ -30,19 +24,13 @@ import {FormHrefComponent} from './components/form-href/form-href.component';
     NgbModule.forRoot(),
   ],
   declarations: [
-    AddItemComponent,
     KeysPipe,
     CardComponent,
     MachineToolSpecificationFormComponent,
     MachiningCapabilityFormComponent,
-    FieldsetComponent,
     RouterOutletComponent,
-    CardActionComponent,
-    ArrayActionComponent,
     ChuckFormComponent,
-    FormGroupComponent,
     InputComponent,
-    SelectComponent,
     FormComponent,
     DeviceIdControlsComponent,
     LocationControlsComponent,
@@ -50,21 +38,15 @@ import {FormHrefComponent} from './components/form-href/form-href.component';
     FormHrefComponent,
   ],
   exports: [
-    AddItemComponent,
     CardComponent,
     MachineToolSpecificationFormComponent,
     KeysPipe,
     ReactiveFormsModule,
     NgbModule,
     MachiningCapabilityFormComponent,
-    FieldsetComponent,
     RouterOutletComponent,
-    CardActionComponent,
-    ArrayActionComponent,
     ChuckFormComponent,
-    FormGroupComponent,
     InputComponent,
-    SelectComponent,
     FormComponent,
     DeviceIdControlsComponent,
     LocationControlsComponent,
