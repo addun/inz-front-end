b430aba18e17db112c1edfa57edd77f24d8edffb
Adrian Bury <kubaadrian2@gmail.com>
2018-02-08 19:17:20 +0100

    Remove tree component

--- src/app/routers/tree/tree.component.ts
+++ src/app/routers/tree/tree.component.ts
@@ -1,172 +1,19 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MenuItemSelectedEvent, NodeEvent, NodeMenuItemAction, Tree, TreeComponent as TC, TreeModel} from 'ng2-tree';
-import {TreeService} from './services/tree/tree.service';
-import {Router} from '@angular/router';
-import {Directory} from './models/directory.model';
-import {MachineToolSpecificationFormService, MachineToolSpecificationService} from '../forms/machine-tool-specification/shared/services';
-import {MachineToolSpecification} from '../forms/shared/models/machine-tool-specification.model';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'inz-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.sass']
 })
-export class TreeComponent implements OnInit, AfterViewInit {
-  @ViewChild('treeFFS') treeFFS: TC;
-  public tree: TreeModel = {
-    value: '/',
-    id: 'root',
-    settings: {
-      'rightMenu': true,
-      isCollapsedOnInit: false,
-      cssClasses: {
-        expanded: 'fa fa-caret-down',
-        collapsed: 'fa fa-caret-right',
-        empty: 'disable',
-        leaf: 'fa'
-      },
-      templates: {
-        node: '<i class="fa fa-folder-o"></i>',
-        leaf: '<i class="fa fa-file-o"></i>',
-        'leftMenu': '<i class="fa fa-navicon fa-lg"></i>'
-      },
-      'menuItems': [
-        {action: NodeMenuItemAction.Rename, name: 'Rename', cssClass: 'fa fa-pencil-square-o'},
-        {action: NodeMenuItemAction.NewFolder, name: 'New folder', cssClass: 'fa fa-folder-o'},
-        {action: NodeMenuItemAction.Remove, name: 'Remove', cssClass: 'fa fa-times'},
-      ]
-    },
-    loadChildren: () => {
-    }
-  };
-  selectedNode: Tree;
-  machineToolSpecifications: any[] = [];
+export class TreeComponent implements OnInit {
 
-  constructor(private treeService: TreeService,
-              private machineToolSpecificationFormService: MachineToolSpecificationFormService,
-              private machineToolSpecificationService: MachineToolSpecificationService,
-              private router: Router) {
+  constructor() {
   }
 
 
   ngOnInit() {
-    this.treeService.getTreeStructure().subscribe(
-      structure => {
-        this.treeFFS.treeModel.children = structure;
-      }
-    );
   }
 
-  ngAfterViewInit(): void {
-    const rootNodeController = this.treeFFS.getControllerByNodeId('root');
-    this.treeService
-      .getTreeStructure()
-      .subscribe(structure => {
-        rootNodeController.setChildren(structure);
-      });
-  }
-
-  onNodeSelected(e: NodeEvent): void {
-    if (e.node.id !== 'root') {
-      this.selectedNode = e.node;
-      this.treeService.getMachineToolSpecificationsInNode(+this.selectedNode.id)
-        .subscribe(data => {
-          this.machineToolSpecifications = data;
-        });
-    } else {
-      this.selectedNode = null;
-    }
-
-  }
-
-
-  onMenuItemSelected(e: MenuItemSelectedEvent): void {
-  }
-
-  onNodeRemoved(e: MenuItemSelectedEvent): void {
-    this.removeNode(e.node);
-  }
-
-  public onNodeRenamed(e: NodeEvent): void {
-    this.patchNode(e.node);
-  }
-
-
-  public onNodeCreated(e: NodeEvent): void {
-    this.saveNode(e.node);
-  }
-
-  removeMachineToolSpecification(id: number) {
-    if (confirm('Are you sure?')) {
-      this.machineToolSpecificationService.removeMachineToolSpecification(
-        +this.machineToolSpecifications[id].id
-      ).subscribe(model => {
-        this.machineToolSpecifications.splice(id, 1);
-      });
-    }
-  }
-
-  editMachineToolSpecification(id: number) {
-    this.machineToolSpecificationFormService.loadMachineToolSpecificationFormFromModel(
-      this.machineToolSpecifications[id].data
-    );
-    this.machineToolSpecificationFormService.machineToolSpecificationId = this.machineToolSpecifications[id].id;
-    this.router.navigate(['/forms/machine-tool-specification/']);
-  }
-
-  addMachineToolSpecifications() {
-    const selectedNodeId = this.selectedNode.id;
-
-    this.machineToolSpecificationFormService.loadMachineToolSpecificationFormFromModel(new MachineToolSpecification({
-      description: 'Empty'
-    }));
-
-    this.machineToolSpecificationService.addMachineToolSpecification(
-      this.machineToolSpecificationFormService.machineToolSpecificationForm.value
-    ).subscribe(element => {
-      this.machineToolSpecifications.push(element);
-
-      this.treeService.addMachineToolSpecificationToNode(+selectedNodeId, element.id)
-        .subscribe();
-    });
-
-  }
-
-  MachineToolSpecificationToXML(i: number) {
-    const xml = json2xml(this.machineToolSpecifications[i].data);
-    const file = `<machine_tool_element>${xml}</machine_tool_element>`;
-
-    download('data.xml', file);
-  }
-
-  private patchNode(node: Tree) {
-    const directory = new Directory({
-      value: node.value,
-      parent: +node.parent.id,
-      id: +node.id
-    });
-    this.treeService
-      .patchNode(directory)
-      .subscribe();
-  }
-
-  private saveNode(node: Tree) {
-    const directory = new Directory({
-      value: node.value,
-      parent: node.parent.id === 'root' ? null : +node.parent.id
-    });
-    this.treeService
-      .addNode(directory)
-      .subscribe(response => {
-        node.id = response.id;
-      });
-  }
-
-  private removeNode(node: Tree) {
-    this.treeService
-      .deleteNode(+node.id)
-      .subscribe();
-  }
 }
 
 /*
--- src/app/routers/tree/tree.module.ts
+++ src/app/routers/tree/tree.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-
 import {TreeRoutingModule} from './tree-routing.module';
 import {TreeComponent} from './tree.component';
 import {TreeService} from './services/tree/tree.service';
