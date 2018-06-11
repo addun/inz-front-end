0a5aca8be86295416fc3fa6077b83c1544179d74
Adrian Bury <kubaadrian2@gmail.com>
2018-06-11 21:25:33 +0200

    Remove breadcrumbs dependency

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './layout.component';
-import {BreadcrumbsModule} from 'ng2-breadcrumbs';
 import {SharedModule} from '../shared/shared.module';
 import {NavbarComponent} from './navbar/navbar.component';
 import {ContentComponent} from './content/content.component';
@@ -10,7 +9,6 @@ import {ContentComponent} from './content/content.component';
   imports: [
     CommonModule,
     SharedModule,
-    BreadcrumbsModule,
   ],
   declarations: [
     LayoutComponent,
